72a940e7cf4050801cfead9c48fd3504f8a29298
Laurent LIN <l.lin@student.isartdigital.com>
2024-05-29 15:12:03 +0200

    start enemy detection

--- Scripts/MushroomEnnemy.gd
+++ Scripts/MushroomEnnemy.gd
@@ -14,19 +14,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
+	# Choose anim
 	if (velocity.x > 1 || velocity.x < -1) :
 		sprite_2d.animation = "Run"
 	else :
 		sprite_2d.animation = "default"
-		
+	
+	# Flip 	
 	if is_on_floor() && (not $RayCast2D.is_colliding() || is_on_wall()):
 		scale.x *= -1
 		direction *= -1	
 		#print("change dir")
 		
+	# Apply gravity
 	if not is_on_floor(): 
 		velocity.y += gravity * delta
 	
+	# Set Velocity
 	if is_on_floor() :
 		velocity.x = direction * speed
 		
@@ -36,3 +40,9 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	pass
+	
+
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
