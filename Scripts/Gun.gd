cfb1a2c20241ed0d8297288dfd53c1a3fa5e234e
b.gomes <b.gomes@student.isartdigital.com>
2024-05-29 17:32:06 +0200

    improved gun

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -2,14 +2,28 @@ extends Node2D
 
 class_name Gun 
 
+@onready var animSprite = $AnimatedSprite2D
 @export var proj : PackedScene
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var parent = get_parent()
+	parent.connect("RotateGun" , rotateOnParent.bind(parent.get_child(1)))
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Shoot") :
-		print("PEW")
+	if Input.is_action_pressed("Shoot") :
+		animSprite.animation = "Shoot"
+		var proj : Projectile = proj.instantiate() as Projectile
+		
+	pass
+
+func _on_animated_sprite_2d_animation_looped():
+	animSprite.animation = "default"
+	pass # Replace with function body.
+
+func rotateOnParent(sprite) :
+	var spriteParent = sprite as Sprite2D
+	animSprite.flip_h = spriteParent.flip_h
 	pass
